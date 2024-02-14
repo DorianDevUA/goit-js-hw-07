6e60b50962221538452f48afed6dafbf09146b63
DorianDevUA <dorian.devua@gmail.com>
2024-02-14 18:04:45 +0400

    Added Homework #7 (Task 1)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+
+function renderGallery(gallery) {
+  const markup = createGalleryMarkup(galleryItems);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+
+renderGallery(galleryList);
+
+galleryList.addEventListener('click', onGalleryItemClick);
+document.addEventListener('keydown', onEscapeKeyPress);
+
+let instance;
+
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  const { target } = evt;
+
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const largeImageURL = target.dataset.source;
+  openLightbox(largeImageURL);
+}
+
+
+function onEscapeKeyPress(evt) {
+  const isLightboxVisible = instance.visible();
+  if (evt.code === 'Escape' && isLightboxVisible) {
+    closeLightbox();
+  }
+}
+
+
+function openLightbox(ImageURL) {
+  instance = basicLightbox.create(`
+    <img src="${ImageURL}" width="800" height="600">
+  `);
+  instance.show();
+}
+
+
+function closeLightbox() {
+  instance.close();
+}
